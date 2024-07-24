2cea7a1ecb962031d8e028532a4781556bb494ac
DerevenetsArtyom <derevenetsartyom@gmail.com>
2024-07-24 22:33:33 +0300

    Copy old code into `goit-pycore-hw-07` and add new fields/features

---
+++ goit-pycore-hw-07/fields/address_book.py
@@ -0,0 +1,56 @@
+from datetime import datetime, timedelta
+from collections import UserDict
+
+from .record import Record
+
+
+class AddressBook(UserDict):
+    """Implementation of basic version of the address book."""
+
+    def add_record(self, record: Record) -> None:
+        """Add the record to the address book."""
+        if record.name.value in self.data:
+            raise KeyError(f"The record with name '{record.name.value}' already exists.")
+
+        self.data[record.name.value] = record
+
+    def find(self, name: str) -> Record:
+        """Find the record by name."""
+        record = self.data.get(name)
+        if not record:
+            raise KeyError(f"The record with name '{name}' is not found.")
+
+        return record
+
+    def delete(self, name: str) -> None:
+        """Delete the record by name."""
+        if name not in self.data:
+            raise KeyError(f"The record with name '{name}' is not found.")
+
+        del self.data[name]
+
+    def get_upcoming_birthdays(self):
+        today = datetime.today().date()
+        upcoming_birthdays = []
+
+        for user in self.data.values():
+            if user.birthday:
+                # Convert the birthday string to a datetime.date object
+                birthday_date = datetime.strptime(user.birthday.value, "%Y.%m.%d").date()
+                # Set the year to the current year to be able to compare with the current date
+                birthday_this_year = birthday_date.replace(year=today.year)
+
+                # If the birthday has passed this year, move it to next year
+                if birthday_this_year < today:
+                    birthday_this_year = birthday_this_year.replace(year=today.year + 1)
+
+                # Get the number of days until the next birthday
+                days_until_birthday = (birthday_this_year - today).days
+
+                if 0 <= days_until_birthday <= 7:
+                    if birthday_this_year.weekday() >= 5:
+                        birthday_this_year += timedelta(days=(7 - birthday_this_year.weekday()))
+
+                    upcoming_birthdays.append(user)
+
+        return upcoming_birthdays
