3be3b791e01f22e9d30ee9b50ce6012e742828d8
DerevenetsArtyom <derevenetsartyom@gmail.com>
2024-12-25 09:55:49 +0200

    Add `hw_5_postgres`

---
+++ 04.Computer-Systems-and-Their-Fundamentals/goit-cs-hw-03/hw_5_postgres/src/db_connection.py
@@ -0,0 +1,33 @@
+"""PostgreSQL connection module"""
+
+from contextlib import contextmanager
+from psycopg2 import connect, DatabaseError
+
+config = {
+    'dbname': 'postgres',
+    'user': 'postgres',
+    'password': 'password12345',
+    'host': 'localhost',
+    'port': '5432'
+}
+
+username = config.get("user", "user")
+password = config.get("password", "pass")
+host = config.get("host", "host")
+db_name = config.get("dbname", "db_name")
+
+
+@contextmanager
+def connection():
+    """Function that provides a connection to the PostgreSQL database."""
+    conn = None
+    try:
+        conn = connect(host=host, user=username, database=db_name, password=password)
+        yield conn
+        conn.commit()
+    except DatabaseError as err:
+        print(err)
+        conn.rollback()
+    finally:
+        if conn:
+            conn.close()
