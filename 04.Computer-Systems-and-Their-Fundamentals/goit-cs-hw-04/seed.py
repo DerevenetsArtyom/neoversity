9612f79c861cc0cbcaf8e7f92beb5dbc154e1706
DerevenetsArtyom <derevenetsartyom@gmail.com>
2024-12-25 20:17:48 +0200

    Add `goit-cs-hw-04`

---
+++ 04.Computer-Systems-and-Their-Fundamentals/goit-cs-hw-04/seed.py
@@ -0,0 +1,27 @@
+"""Module to create and populate files with randomly generated text in the specific folder"""
+
+import os
+from faker import Faker
+
+FILES = 50
+LENGTH = 2000
+DIRECTORY = "src"
+
+fake = Faker()
+
+
+def main(num_files: int = FILES, text_length: int = LENGTH, directory: str = DIRECTORY):
+    """Function to create a directory and populate it with files with fake text"""
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    for i in range(1, num_files+1):
+        file_path = os.path.join(directory, f"file{i}.txt")
+        with open(file_path, "w", encoding="UTF-8") as fd:
+            fd.write(fake.text(max_nb_chars=text_length))
+
+    print(f"Seeded {num_files} files with random text in the '{directory}' directory")
+
+
+if __name__ == "__main__":
+    main()
